3fe6dd91c8605b0440c910b2ee24a7f340234aaa
Khushi-Bagadia <khushibagadia9@gmail.com>
2025-06-25 16:20:37 +0530

    Add files via upload

---
+++ HTML_CSS_Tasks/HTML/list_task.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning Web Development</title>
+</head>
+<body>
+
+    <h1>Learning Web Development</h1>
+    <ol type="I">
+
+        <li>Background Skills</li>
+            <ol type="A">
+                <li>Unix commands</li>
+                <li>Vim Text Editor</li>
+            </ol>
+
+        <li>HTML</li>
+            <ol type="A">
+                <li>Minimal Page</li>
+                <li>Headings</li>
+                <li>Tags</li>
+                <li>Lists</li>
+                    <ol type="i">
+                        <li>Unordered</li>
+                        <li>Ordered</li>
+                        <li>Definition</li>
+                        <li>Nested</li>
+                    </ol>
+                <li>Links</li>
+                    <ol type="i">
+                        <li>Absolute</li>
+                        <li>Relative</li>
+                    </ol>
+                <li>Images</li>
+            </ol>
+
+        <li>CSS</li>
+            <ol type="A">
+                <li>Anatomy</li>
+                <li>Basic Selectors</li>
+                    <ol type="i">
+                        <li>Element</li>
+                        <li>Class</li>
+                        <li>ID</li>
+                        <li>Group</li>
+                    </ol>
+                <li>The DOM</li>
+                <li>Advanced Selectors</li>
+                <li>Box model</li>
+            </ol>
+
+        <li>Programming</li>
+            <ol type="A">
+                <li>Python</li>
+                <li>Javascript</li>
+            </ol>
+
+        <li>Database</li>
+            <ol type="A">
+                <li>Flat File</li>
+                <li>Relational</li>
+            </ol>
+
+    </ol>
+
+    <br>
+
+    <a href="http://localhost/HTML_CSS_Tasks/HTML/list_task.php" target="_blank"><h3 style="text-align:center">HTML Valid!</h3></a>
+</body>
+</html>
